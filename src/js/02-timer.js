b546a9e967b0108a27f73c3253247f09f4438257
Oanasuciu97 <luciana_bodeaoana@yahoo.com>
2024-05-03 17:39:59 +0300

    npm set up

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,7 +1,6 @@
 import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
 import Notiflix from 'notiflix';
-
 //For style
 const timerContainer = document.querySelector('.timer');
 timerContainer.style.display = 'flex';
